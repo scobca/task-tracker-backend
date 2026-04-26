4321e6eaf6988e00e8177be60762a18dbf5161a0
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-04-26 18:01:33 +0300

    docs(locales): added comments about messageSource using

--- src/main/kotlin/org/esc/tasktracker/LocaleTestClass.kt
+++ src/main/kotlin/org/esc/tasktracker/LocaleTestClass.kt
@@ -21,6 +21,15 @@ class LocaleTestClass(
     @Value($$"${spring.locales.default}")
     private lateinit var defaultLocale: String
 
+    /**
+     * `messageSource.getMessage("response.ok", null, Locale.of(defaultLocale))` - это метод, который получает текст из бандла
+     *
+     * **arg1:** response.ok — это <бандл.строка>, которую ты хочешь вытащить. Бандлов всего 2: response и error, язык бандла указывать не надо, spring сделает это автоматически
+     * **arg2:** null — так надо, просто не трогаем
+     * **arg3:** Locale.of(defaultLocale) — получение экземпляра класса Locale из дефолтной конфигурации приложения, также просто оставляем так, не меняем
+     *
+     * **Обобщение** — по сути единственное, что надо менять это arg1, в нем указываем какой мы будем бандл использовать и какую строку из этого бандла будем тянуть
+     */
     fun logMessage() {
         println(messageSource.getMessage("response.ok", null, Locale.of(defaultLocale)))
     }
